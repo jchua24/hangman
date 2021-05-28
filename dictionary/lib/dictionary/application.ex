563aff13df5d0df0483986065910ec00cb4a7703
Joshua Chua <winwin.chua@gmail.com>
2021-05-28 10:17:57 -0400

    added supervisor to dictionary application

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -4,7 +4,19 @@ defmodule Dictionary.Application do
 
   #callback that starts the application process
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+
+    import Supervisor.Spec #supervisor helpers
+
+    #Dictionary.WordList.start_link()
+
+    children = [worker(Dictionary.WordList, [])] #list of worker processes
+
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one #each process this supervisor is monitoring is independent
+    ]
+
+    Supervisor.start_link(children, options)
   end
 
 end
